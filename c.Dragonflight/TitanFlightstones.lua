70f6de04ce5bf835ac65644dcc30821c73682c51
Paulo <paulo@canetti.net>
2023-05-12 17:01:43 -0300

    (Flightstones) added
    (toc file) Updated to Dragonflight 10.1

---
+++ c.Dragonflight/TitanFlightstones.lua
@@ -0,0 +1,18 @@
+--[[
+Description: This plugin is part of the "Titan Panel [Currencies] Multi" addon. It shows your Flightstones.
+Site: https://www.curseforge.com/wow/addons/titan-panel-currencies-multi
+Author: Canettieri
+Special Thanks to Eliote.
+--]]
+
+local _, L = ...;
+local ID = "TITAN_FLHTSM"
+local CURRENCY_ID = 2245
+
+L:CreateSimpleCurrencyPlugin({
+	currencyId = CURRENCY_ID,
+	titanId = ID,
+	noCurrencyText = L["DragonFOnly"],
+	expName = L["mDragonflight"],
+	category = "CATEGORY_DRAGON"
+})
