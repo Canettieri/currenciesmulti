c01ac4d86a214e69a017eb085f87a5fdf3fa6da2
Protuhj <ihatespam234@gmail.com>
2024-08-23 00:25:17 -0400

    Adds Sizzling Cinderpollen item
    Need 1400 total apparently.

---
+++ c.WarWithin/TitanSizzlingCinderpollen.lua
@@ -0,0 +1,17 @@
+--[[
+Description: This plugin is part of the "Titan Panel [Currencies] Multi" addon. It shows the Sizzling Cinderpollen item.
+Site: https://www.curseforge.com/wow/addons/titan-panel-currencies-multi
+Author: Canettieri
+Special Thanks to Protuhj.
+--]]
+
+local _, L = ...;
+
+-- Sizzling Cinderpollen
+L:CreateSimpleItemPlugin({
+	itemId = 225557,
+	titanId = "TITAN_SIZZLINGCPOLLEN",
+	noCurrencyText = L["NoTWWCurrency"],
+	expName = L["mWarWithin"],
+	category = "CATEGORY_TWW"
+})
