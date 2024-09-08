a73b9689725f3c46f04b60162fbbc0398d43f865
Protuhj <ihatespam234@gmail.com>
2024-09-07 21:07:11 -0400

    Adds the Radiant Echo item

---
+++ c.WarWithin/TitanRadiantEchoes.lua
@@ -0,0 +1,17 @@
+--[[
+Description: This plugin is part of the "Titan Panel [Currencies] Multi" addon. It shows the Radiant Echo item.
+Site: https://www.curseforge.com/wow/addons/titan-panel-currencies-multi
+Author: Canettieri
+Special Thanks to Protuhj.
+--]]
+
+local _, L = ...;
+
+-- Radiant Echo
+L:CreateSimpleItemPlugin({
+	itemId = 220520,
+	titanId = "TITAN_RADECHO",
+	noCurrencyText = L["NoTWWCurrency"],
+	expName = L["mWarWithin"],
+	category = "CATEGORY_TWW"
+})
