b4abe39a25d219f18a462f43643ecc5cbfedb170
Paulo <paulo@canetti.net>
2024-09-10 17:33:38 -0300

    (Undercoin) added

---
+++ c.WarWithin/TitanUndercoin.lua
@@ -0,0 +1,16 @@
+--[[
+Description: This plugin is part of the "Titan Panel [Currencies] Multi" addon. It shows the Undercoin currency.
+Site: https://www.curseforge.com/wow/addons/titan-panel-currencies-multi
+Author: Canettieri
+Special Thanks to Eliote.
+--]]
+
+local _, L = ...;
+
+L:CreateSimpleCurrencyPlugin({
+	currencyId = 2803,
+	titanId = "TITAN_UNDRCNM",
+	noCurrencyText = L["NoTWWCurrency"],
+	expName = L["mWarWithin"],
+	category = "CATEGORY_TWW",
+})
