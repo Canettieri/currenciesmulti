97477f3933cf2e2bd40456a8afbfd653bc6fbf71
NayooZ <37068647+NayooZ@users.noreply.github.com>
2025-02-19 11:29:07 +1100

    Add Obisidian Stone Fragments
    New currency added in 4.4.2.59185 for Twilight Protocol Dungeons

---
+++ c.Legacy/Cataclysm/TitanObsidianStoneFragment.lua
@@ -0,0 +1,18 @@
+--[[
+Description: This plugin is part of the "Titan Panel [Currencies] Multi" addon. It shows your amount of Obsidian Stone Fragments.
+Site: https://www.curseforge.com/wow/addons/titan-panel-currencies-multi
+Author: Canettieri
+Special Thanks to Eliote.
+--]]
+
+local _, L = ...;
+local ID = "TITAN_OSFRGM"
+local CURRENCY_ID = 3281
+
+L:CreateSimpleCurrencyPlugin({
+	currencyId = CURRENCY_ID,
+	titanId = ID,
+	noCurrencyText = L["NoCataCurrency05"],
+	expName = L["mCata"],
+	category = "CATEGORY_CATA"
+})
