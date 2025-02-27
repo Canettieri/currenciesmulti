4e373e002c4016670c78da61b2b954d403cc9597
Protuhj <ihatespam234@gmail.com>
2025-02-27 02:30:04 -0500

    Initial currencies for the Undermine zone

---
+++ c.WarWithin/TitanUndermine.lua
@@ -0,0 +1,46 @@
+--[[
+Description: This plugin is part of the "Titan Panel [Currencies] Multi" addon.
+It shows currencies associated with the Undermine patch and zone.
+Site: https://www.curseforge.com/wow/addons/titan-panel-currencies-multi
+Author: Canettieri
+Special Thanks to Protuhj.
+--]]
+
+local _, L = ...;
+
+-- Empty Kaja'Cola Can
+L:CreateSimpleCurrencyPlugin({
+	currencyId = 3218,
+	titanId = "TITAN_KJCOLACAN",
+	noCurrencyText = L["TWWUndermine"],
+	expName = L["mUndermine"],
+	category = "CATEGORY_TWW"
+})
+
+-- Vintage Kaja'Cola Can
+L:CreateSimpleCurrencyPlugin({
+	currencyId = 3220,
+	titanId = "TITAN_VINTCOLACAN",
+	noCurrencyText = L["TWWUndermine"],
+	expName = L["mUndermine"],
+	category = "CATEGORY_TWW"
+})
+
+-- Gunk-Covered Thingy
+L:CreateSimpleItemPlugin({
+	itemId = 233246,
+	titanId = "TITAN_GCTHINGY",
+	noCurrencyText = L["TWWUndermine"],
+	expName = L["mUndermine"],
+	category = "CATEGORY_TWW"
+})
+
+-- Miscellaneous Mechanica
+L:CreateSimpleItemPlugin({
+	itemId = 234741,
+	titanId = "TITAN_MISCMECH",
+	noCurrencyText = L["TWWUndermine"],
+	expName = L["mUndermine"],
+	category = "CATEGORY_TWW",
+	allowAccountTotal = true
+})
