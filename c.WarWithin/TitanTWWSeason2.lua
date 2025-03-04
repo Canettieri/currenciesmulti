5975eb72c0864cc6ae9c5b332aacdc86326f170b
Paulo <paulo@canetti.net>
2025-03-04 02:45:05 -0300

    (TWW Season 2) Added new crests

---
+++ c.WarWithin/TitanTWWSeason2.lua
@@ -0,0 +1,49 @@
+--[[
+Description: This plugin is part of the "Titan Panel [Currencies] Multi" addon.
+It shows the seasonal currencies from The War Within Season 2.
+Site: https://www.curseforge.com/wow/addons/titan-panel-currencies-multi
+Author: Canettieri
+Special Thanks to Odysseas68.
+--]]
+
+local _, L = ...;
+
+-- Weathered Undermine Crests
+L:CreateSimpleCurrencyPlugin({
+	currencyId = 3107,
+	titanId = "TITAN_WEATHUNDECREST",
+	noCurrencyText = L["TWWSeason2"],
+	expName = L["mWarWithinS2"],
+	category = "CATEGORY_TWW",
+	forceMax = true
+})
+
+-- Carved Undermine Crests
+L:CreateSimpleCurrencyPlugin({
+	currencyId = 3108,
+	titanId = "TITAN_CARVEDUNDECREST",
+	noCurrencyText = L["TWWSeason2"],
+	expName = L["mWarWithinS2"],
+	category = "CATEGORY_TWW",
+	forceMax = true
+})
+
+-- Runed Undermine Crests
+L:CreateSimpleCurrencyPlugin({
+	currencyId = 3109,
+	titanId = "TITAN_RUNEDUNDECREST",
+	noCurrencyText = L["TWWSeason2"],
+	expName = L["mWarWithinS2"],
+	category = "CATEGORY_TWW",
+	forceMax = true
+})
+
+-- Gilded Undermine Crests
+L:CreateSimpleCurrencyPlugin({
+	currencyId = 3110,
+	titanId = "TITAN_GILDUNDECREST",
+	noCurrencyText = L["TWWSeason2"],
+	expName = L["mWarWithinS2"],
+	category = "CATEGORY_TWW",
+	forceMax = true
+})
