981bde2ebce7d11c7665eded0d39184fed419330
Protuhj <1165219+Protuhj@users.noreply.github.com>
2025-08-05 21:29:49 -0400

    Adding TWW S3 currencies
    Note that TWW S2 catalyst charges are still relevant in case you want to catalyze old gear
    
    Also note that Runed and Gilded crests cannot be earned yet, so do not show a max value on the bar.

--- Core/languages/ptBR.lua
+++ Core/languages/ptBR.lua
@@ -14,6 +14,7 @@ L["No20thCurrency"] = "Pode ser obtida até o dia 7 de\rjaneiro de 2025 no event
 L["NoTWWCurrency"] = "Obtida em The War Within."
 L["NoTWWCurrency01"] = "Obtida através do evento de\rpré-patch de The War Within."
 L["TWWSeason2"] = "Obtida na temporada 2 de The War\rWithin."
+L["TWWSeason3"] = "Obtida na temporada 3 de The War\rWithin."
 L["TWWUndermine"] = "Obtida na Inframina."
 
 ------ Dragonflight pack
@@ -189,6 +190,7 @@ L["mShadowlands"] = "[|cFF198ae0Shadowlands|r]"
 L["mDragonflight"] = "[|cFF198ae0Dragonflight|r]"
 L["mWarWithin"] = "[|cFF198ae0The War Within|r]"
 L["mWarWithinS2"] = "[|cFF198ae0The War Within (S2)|r]"
+L["mWarWithinS3"] = "[|cFF198ae0The War Within (S3)|r]"
 L["mUndermine"] = "[|cFF198ae0Inframina|r]"
 L["mMoPRemix"] = "[|cFF198ae0MoP Remix|r]"
 L["info"] = "|cFFFFFFFF[Informações]|r"
